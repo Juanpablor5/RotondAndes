c0de31600baef995eefd4e26af23823b0a4e857e
JuanCamiloBojaca <30963726+JuanCamiloBojaca@users.noreply.github.com>
2017-10-03 11:48:22 -0500

    crud

--- src/dao/CRUD.java
+++ src/dao/CRUD.java
@@ -1,5 +1,12 @@
 package dao;
 
-public interface CRUD {
+import java.sql.SQLException;
+import java.util.List;
 
+public interface CRUD<T> {
+	public T get(long id)throws SQLException, Exception;
+	public List<T> getAll()throws SQLException, Exception;
+	public void add(T t)throws SQLException, Exception;
+	public void update(T t)throws SQLException, Exception;
+	public void delete(T t)throws SQLException, Exception;
 }
--- src/dao/DAORegistro.java
+++ src/dao/DAORegistro.java
@@ -1,16 +1,27 @@
 package dao;
 
+import java.sql.SQLException;
+
 import vos.Registro;
 
-public class DAORegistro extends DAOBase{
+public class DAORegistro extends DAOBase implements CRUD<Registro>{
 	private final static String TABLA="REGISTRO";
 	
-	public void add(Registro registro) {
-		String sql = "INSERT INTO VIDEO VALUES (";
-		sql += video.getId() + ",'";
-		sql += video.getName() + "',";
-		sql += video.getDuration() + ")";
+	/**
+	 * metodo que agrega un registro a la base de datos
+	 * @param data los datos a agregar
+	 * * @throws SQLException - Cualquier error que la base de datos arroje. No pudo agregar el video a la base de datos
+	 * @throws Exception - Cualquier error que no corresponda a la base de datos
+	 */
+	public void add(Registro data) throws SQLException, Exception {
+		String sql = "INSERT INTO "+TABLA+" VALUES (";
+		sql += data.getCodigo() + ",'";
+		sql += data.getUsuario() + "',";
+		sql += data.getContrasena() + "',";
+		sql += data.getPermisos() + ")";
 		
 		executeModification(sql);
 	}
+	
+	
 }
