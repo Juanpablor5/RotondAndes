eaec3f03e07d46ea5673bea48e42de08d657b50e
JuanCamiloBojaca <30963726+JuanCamiloBojaca@users.noreply.github.com>
2017-10-03 20:32:53 -0500

    urls

--- src/rest/URLS.java
+++ src/rest/URLS.java
@@ -7,4 +7,16 @@ public interface URLS {
 	final static String CLIENTEID="clientes_id";
 	final static String ZONA="zonas";
 	final static String ZONAID="zonas_id";
+	final static String RESTAURANTE="restaurantes";
+	final static String RESTAURANTEID="restaurantes_id";
+	final static String PRODUCTO="productos";
+	final static String PRODUCTOID="productos_id";
+	final static String INGREDIENTE="ingredientes_id";
+	final static String INGREDIENTEID="ingredientes_id";
+	final static String MENU="menus";
+	final static String MENUID="menus_id";
+	final static String PREFERECIAS="preferencias";
+	final static String PREFERECIASID="preferencias_id";
+	final static String PEDIDO="pedido";
+	final static String PEDIDOID="pedido_id";
 }
